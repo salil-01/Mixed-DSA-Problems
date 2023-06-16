4f9cbcdf3e2ab5d47972cd63d99dc0858311b952
Salil Upadhyay <upsalil.01@gmail.com>
2023-06-16 09:34:34 +0530

    mock13 round1

---
+++ revision/oddAdjacentElement.js
@@ -0,0 +1,37 @@
+// Odd Adjacent Elements Ended
+// Description
+
+// Given a matrix A, you have to transform the given matrix such that you have to make A[i][j] = -A[i][j] if its 2 or more adjacent elements are odd.
+
+// Input
+// Input Format
+
+// First-line contains 2 integers n and m representing the number of rows and columns respectively of 2D array arr.
+
+// Following n lines contains m-integers.
+
+// Constraints
+
+// 1 <= n <= 100
+
+// 1 <= m <= 100
+
+// 1 <= arr[i][j] <= 10^4
+
+// Output
+// Print the resultant matrix.
+
+// Sample Input 1
+
+// 3 3
+// 1 2 3
+// 4 5 6
+// 7 8 9
+// Sample Output 1
+
+// 1 -2 3
+// -4 5 -6
+// 7 -8 9
+// Hint
+
+// Element 2,4,6,and 8 are surrounded by 2 or more odd elements.
