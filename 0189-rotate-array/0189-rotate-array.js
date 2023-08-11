a95bb72b1b93c04436a6e1e647e1165e11b0f1ff
Salil Upadhyay <upsalil.01@gmail.com>
2023-08-11 09:12:24 +0530

    Time: 77 ms (92.75%), Space: 51.2 MB (80.26%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    let timeRotated = k % nums.length;
+    nums.unshift(...nums.splice(-timeRotated))
+};
