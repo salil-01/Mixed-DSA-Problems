0ae17cc1366d11eb01651cc3e5c18959b9ae2f88
Salil Upadhyay <salil@ripik.ai>
2024-05-09 21:22:09 +0530

    Time: 281 ms (7.53%) | Memory: 71.7 MB (11.19%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.js
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.js
@@ -3,29 +3,23 @@
  * @param {number} k
  * @return {number}
  */
+
+// brute force 
 var maximumHappinessSum = function(happiness, k) {
     happiness = happiness.sort((a,b)=>b-a);
     let ans = 0;
+    
+    console.log(happiness)
     for(let i=0; i<k; i++){
-      ans += Math.max(happiness[i] - i, 0)
+        
+    
+        if(happiness[i]>0){
+            let temp = happiness[i] - i
+             if(temp > 0){
+                ans += temp
+             }
+        }
+        
     }
     return ans;
 };
-
-// brute force 
-// var maximumHappinessSum = function(happiness, k) {
-//     happiness = happiness.sort((a,b)=>b-a);
-//     let ans = 0;
-//     for(let i=0; i<k; i++){
-//         ans += happiness[0];
-//         happiness.shift();
-//         for(let j=0; j<happiness.length; j++){
-//             if(happiness[j]>0){
-//                 happiness[j] = happiness[j] - 1; 
-//             }else{
-//                 break;
-//             }
-//         }
-//     }
-//     return ans;
-// };
