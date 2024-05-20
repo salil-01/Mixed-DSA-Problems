5521a7357068b89525bae36b150c2bd0f50c798b
Salil Upadhyay <salil@ripik.ai>
2024-05-21 00:14:58 +0530

    Time: 668 ms (5.08%) | Memory: 95.8 MB (17.04%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeNodes = function(head) {
+    console.log(head)
+    let curr = reverseLinkedList(head);
+    let temp = curr;
+    while(curr && curr.next){
+        if(curr.next.val < curr.val){
+              // if the next value is smaller than skipping or removing that node
+              curr.next = curr.next.next;
+        }else{
+              // just moving one step ahead
+              curr = curr.next;
+        }
+    }
+    // console.log(curr,temp)    
+    let res = reverseLinkedList(temp);
+    return res;
+};
+function reverseLinkedList(head){
+    let current  = head;
+    let past = null; 
+    let future = null;
+
+    while(current){
+        future = current.next;
+        current.next = past;
+        past = current;
+        current = future;
+    }
+    head = past;
+    return head;
+}
