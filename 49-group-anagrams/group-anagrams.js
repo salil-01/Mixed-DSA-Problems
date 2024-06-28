226d2335e0ee085ad4ea484814e3ccd9d14f8373
Salil Upadhyay <salil@ripik.ai>
2024-06-28 12:31:22 +0530

    Time: 108 ms (75.80%) | Memory: 61.6 MB (89.39%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,19 +3,14 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    let obj = {};
-    for(let i=0; i<strs.length; i++){
+    let map = new Map();
+    for (let i = 0; i < strs.length; i++) {
         let sortedString = strs[i].split("").sort().join("");
-        if(!obj[sortedString]){
-            obj[sortedString] = [strs[i]]
-        }else{
-            obj[sortedString].push(strs[i])
+        if (!map.has(sortedString)) {
+            map.set(sortedString, [strs[i]]);
+        } else {
+            map.get(sortedString).push(strs[i]);
         }
     }
-    // console.log(obj)
-    let res = []
-    for(let key in obj){
-        res.push(obj[key]);
-    }
-    return res;
+    return Array.from(map.values());
 };
