b178c141bdeb539ab546d5563a85145979e1789c
Salil Upadhyay <salil@ripik.ai>
2024-07-05 21:40:09 +0530

    Time: 149 ms (59.57%) | Memory: 82 MB (21.28%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.js
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.js
@@ -13,10 +13,15 @@ var nodesBetweenCriticalPoints = function(head) {
     let current = head;
     let position = [];
     let currentPosition = 1;
+
     while(current.next.next){
+
+        // checking local minima and pushing in array
         if(current.next.val < current.val && current.next.val < current.next.next.val){
             position.push(currentPosition)
         }
+
+        // checking local maxima and pushing its position in array
         if(current.next.val > current.val && current.next.val > current.next.next.val){
             position.push(currentPosition)
         }
@@ -26,6 +31,7 @@ var nodesBetweenCriticalPoints = function(head) {
     // console.log(position)
     let minDistance = Infinity;
 
+    //  to calculte the minDistance between any position whose value are critical
     for(let i=1; i<position.length; i++){
         let tempDistance = position[i]-position[i-1];
         if(tempDistance<minDistance){
@@ -34,14 +40,14 @@ var nodesBetweenCriticalPoints = function(head) {
     }
 
     let maxDistance = -1
+    // calc max only if postion array length greater than 1 
     if(position.length>1){
         maxDistance = position[position.length-1] - position[0]
     }
 
     if(minDistance===Infinity){
+        // if there was no minDistance than return -1
         return [-1, maxDistance]
     }
     return [minDistance,maxDistance]
-
-
 };
