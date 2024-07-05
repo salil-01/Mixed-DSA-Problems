8da6faf363af31c56951fc59b5927fb2105472dc
Salil Upadhyay <salil@ripik.ai>
2024-07-05 20:55:58 +0530

    Time: 127 ms (22.76%) | Memory: 58.7 MB (15.90%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.js
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.js
@@ -6,6 +6,7 @@ var minDifference = function(nums) {
     if(nums.length <= 4) return 0;
     nums  = nums.sort((a,b)=>a-b); // asc sorted
 
+    // there will be atmost four cases :p
     bigThreeDiff = nums[nums.length-4] - nums[0]   // after removing big three
     smallThreeDiff = nums[nums.length-1] - nums[3] // after removing small three
     oneSmallTwoBigDiff = nums[nums.length-3] - nums[1] // removing one small two big
