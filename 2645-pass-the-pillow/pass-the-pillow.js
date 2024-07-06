e99f314e9d994ea08caa8297b4ca6a39ee51dfc8
Salil Upadhyay <salil@ripik.ai>
2024-07-06 18:34:51 +0530

    Time: 50 ms (68.29%) | Memory: 48.9 MB (31.71%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.js
+++ 2645-pass-the-pillow/pass-the-pillow.js
@@ -3,25 +3,36 @@
  * @param {number} time
  * @return {number}
  */
-var passThePillow = function(n, time) {
-    let currentPos = 1;
-    let direction = 1; // 0 for reverse manner 
+
+//   loop solution
+// var passThePillow = function(n, time) {
+//     let currentPos = 1;
+//     let direction = 1; // 0 for reverse manner 
      
-     while(time){
-        if(direction){
-            currentPos++;
-            time--;
-            if(currentPos === n){
-                direction = 0;
-            }
-        }else{
-            currentPos--;
-            time--
-            if(currentPos===1){
-                direction = 1;
-            }
-        }
-     }
-     return currentPos;
-     console.log(currentPos)
+//      while(time){
+//         if(direction){
+//             currentPos++;
+//             time--;
+//             if(currentPos === n){
+//                 //reached the last point reverse the direction
+//                 direction = 0;
+//             }
+//         }else{
+//             currentPos--; //position will be substracted in reverse manner 
+//             time--;
+//             if(currentPos===1){
+//                 // reverse again we reached the first point
+//                 direction = 1;
+//             }
+//         }
+//      }
+//      return currentPos;
+//     //  console.log(currentPos)
+// };
+
+
+// mathematical solm
+var passThePillow = function(n, time) {
+    let chunks = Math.floor(time / (n - 1));
+    return chunks % 2 === 0 ? (time % (n - 1) + 1) : (n - time % (n - 1));
 };
