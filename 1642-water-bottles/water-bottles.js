1f3e65281c986de5e7c6cb58407b5ae0feb78e49
Salil Upadhyay <salil@ripik.ai>
2024-07-08 13:40:09 +0530

    Time: 52 ms (50.83%) | Memory: 49 MB (10.00%) - LeetSync

--- 1642-water-bottles/water-bottles.js
+++ 1642-water-bottles/water-bottles.js
@@ -4,19 +4,25 @@
  * @return {number}
  */
 var numWaterBottles = function(numBottles, numExchange) {
-    let total = numBottles;
-    while(numBottles >= numExchange){
-        let newFilledBottles = Math.floor(numBottles/numExchange);  
-        if(numBottles % numExchange === 0){
-            // means we can get exchange all the bottles
-            total += newFilledBottles;
-            numBottles  =  newFilledBottles;
-        }else{
-            let remainingEmptyBottles = numBottles % numExchange; // remaining empty bottles which can't be exchanged
-            total +=  newFilledBottles
-            numBottles  =  newFilledBottles + remainingEmptyBottles;
-        }
-    }
-    // console.log(total, numBottles)
-    return total;
+
+    // iterative solution
+    // let total = numBottles;
+    // while(numBottles >= numExchange){
+    //     let newFilledBottles = Math.floor(numBottles/numExchange);  
+    //     if(numBottles % numExchange === 0){
+    //         // means we can get exchange all the bottles
+    //         total += newFilledBottles;
+    //         numBottles  =  newFilledBottles;
+    //     }else{
+    //         let remainingEmptyBottles = numBottles % numExchange; // remaining empty bottles which can't be exchanged
+    //         total +=  newFilledBottles
+    //         numBottles  =  newFilledBottles + remainingEmptyBottles;
+    //     }
+    // }
+    // // console.log(total, numBottles)
+    // return total;
+
+
+    // mathematical solution 
+    return (numBottles + Math.floor((numBottles-1)/(numExchange-1)))
 };
