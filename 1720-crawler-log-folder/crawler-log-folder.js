e0d4a3b9978d246c01427ac484429c8df93d75bb
Salil Upadhyay <salil@ripik.ai>
2024-07-10 20:16:42 +0530

    Time: 52 ms (71.07%) | Memory: 49.6 MB (48.76%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} logs
+ * @return {number}
+ */
+var minOperations = function(logs) {
+    // using a variable to maintain where are we
+    let initial = 0;
+    for(let i=0; i<logs.length; i++){
+        if(logs[i] === "../" && initial>0){
+            initial--
+        }else if(logs[i]!=="../" && logs[i]!=="./"){
+            initial++
+        }
+    }
+    console.log(initial)
+    return initial
+};
