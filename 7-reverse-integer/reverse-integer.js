06d26cc8a64d29e2f10470dcd3b5936a9ecf8db5
Salil Upadhyay <salil@ripik.ai>
2024-07-12 23:19:28 +0530

    Time: 76 ms (49.42%) | Memory: 52.9 MB (45.73%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -9,16 +9,19 @@ var reverse = function(x) {
     let strNum = x.toString();
     let res = "";
     if(strNum[0]==="-"){
+        // adding - in start if it's in start even after reversing
         res += "-"
     }
     for(let i=strNum.length-1; i>=0; i--){
         if(strNum[i]==="-"){
             continue;
+            // as - will be at last if there was any in string we will just omit 
         }
         res += strNum[i];
 
     }
     res = Number(res);
+    // checking they are under 32 bit else returning 0
     if(res > INT_MAX) return 0;
     if(res < INT_MIN) return 0;
     return res
