af13febd3e4c869213a5d45e2995946386e203ae
Salil Upadhyay <salil@ripik.ai>
2024-07-13 00:45:10 +0530

    Time: 47 ms (79.07%) | Memory: 48.7 MB (65.65%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -25,6 +25,7 @@ function mergeStrings(str1, str2) {
     return res;
 }
 var letterCombinations = function (digits) {
+    // mappin g of the keyboard
     let keyboard = {
         2: "abc",
         3: "def",
@@ -38,8 +39,11 @@ var letterCombinations = function (digits) {
 
     let strArr = []
     for (let i = 0; i < digits.length; i++) {
+        // pushing all the chars of digit with the help of map
         strArr.push(keyboard[digits[i]])
     }
+
+    // some speciifc conditions check
     if (strArr.length === 1) {
         let res = [];
         for (let i = 0; i < strArr[0].length; i++) {
@@ -50,29 +54,40 @@ var letterCombinations = function (digits) {
     if (strArr.length === 0) {
         return [];
     }
+
     while (strArr.length >= 2) {
+        //running this loop until there are 0 or 1 ele
         let firstElement = strArr[0];
         let secondElement = strArr[1];
 
 
         if (typeof firstElement === "string" && typeof secondElement === "string") {
+            // if bnoth the el are strings merging them and removing those
+            // strings from strArr
             let resultArr = mergeStrings(firstElement, secondElement);
             strArr.shift();
             strArr.shift();
 
             if (strArr.length === 0) {
+                // after removing the first two el, if there are no el remaining
+                // simply we got the answer
                 return resultArr
             } else {
+                //else puttimng as the first element
                 strArr.unshift(resultArr);
             }
         } else {
+            // means first element is arr and other is string, merging them
+            // also removing those from strArr
             let resultArr = mergeArrayWithString(firstElement, secondElement);
             strArr.shift();
             strArr.shift();
 
             if (strArr.length === 0) {
+                // if there are no elements left we got the answer
                 return resultArr;
             } else {
+                // else adding the new array as first element in strArr
                 strArr.unshift(resultArr)
             }
 
