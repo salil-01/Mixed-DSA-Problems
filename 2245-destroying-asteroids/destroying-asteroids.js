3d5e22cfaba94d2818ae6029dc0b8b07c006017e
Salil Upadhyay <salil@ripik.ai>
2024-07-15 11:04:55 +0530

    Time: 178 ms (21.13%) | Memory: 63 MB (97.18%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.js
+++ 2245-destroying-asteroids/destroying-asteroids.js
@@ -4,14 +4,17 @@
  * @return {boolean}
  */
 var asteroidsDestroyed = function (mass, asteroids) {
+    // sorting in descending order
     asteroids = asteroids.sort((a, b) => b - a);
 
     while (asteroids.length > 0 && asteroids[asteroids.length - 1] <= mass) {
+        // checking if mass is equal or less add it mass and remove from asteriod
         mass += asteroids[asteroids.length - 1];
         asteroids.pop()
     }
     // console.log(asteroids, mass)
     if (asteroids.length === 0) {
+        // if there are no asteriod left return true
         return true
     }
     return false
